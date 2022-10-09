1457182bcdf5ef377fd4150d48f3e5cc8587c349
Lucashmcastro <lucashmcastro@outlook.com>
2022-10-09 15:40:46 -0300

    Criação do Server

---
+++ Atividades/atividade-pratica-01-server/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+import { mainRouter } from './routes/main.js';
+import { cidadeRouter } from './routes/cidades.js';
+import { userRouter } from './routes/users.js';
+import { estadoRouter } from './routes/estados.js';
+
+import cors from 'cors';
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5555;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(mainRouter);
+app.use(estadoRouter);
+app.use(cidadeRouter);
+app.use(userRouter);
+
+app.listen(PORT, () =>{
+    console.log(`[SERVER] Servidor rodando na porta ${PORT}`);
+})
