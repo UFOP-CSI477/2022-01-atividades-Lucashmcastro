44326aaaa7b239013dc18681563e0547cb867a3f
Lucashmcastro <lucashmcastro@outlook.com>
2022-10-09 23:28:44 -0300

    Request Cidade finalizado

--- Atividades/atividade-pratica-01-server/app.js
+++ Atividades/atividade-pratica-01-server/app.js
@@ -7,12 +7,21 @@ const app = express();
 app.use(json());
 
 let estados = [];
+let cidades = [];
 
 readFile("estados.json", "utf-8", (err, data) => {
     if (err) {
         console.log(err)
     }else {
         estados = JSON.parse(data);
+    }
+})
+
+readFile("cidades.json", "utf-8", (err, data) => {
+    if (err) {
+        console.log(err)
+    }else {
+        cidades = JSON.parse(data);
     }
 })
 
@@ -31,8 +40,10 @@ Params => /products/13123123123123
 Query => /products?id=234234234234234234&value=234234234234
 */
 
+//metodos para estados
+
 app.post("/estados", (request, response) => {
-    //nome e sigla => name  and price
+    //nome e sigla => name  and sigla
 
     const { name, sigla } = request.body;
 
@@ -94,6 +105,70 @@ function estadoFile() {
             console.log("Estado Inserido");
         }
     });
-}
+};
+
+//metodos para cidades
+
+
+app.post("/cidades", (request, response) => {
+
+    const { name, estado_id } = request.body;
+    const cidade = { 
+        name, 
+        estado_id, 
+        id: randomUUID(),
+    }
+
+    cidades.push(cidade);
+    cidadeFile();
+    return response.json(cidade);
+});
+
+app.get("/cidades", (request, response) =>{
+    return response.json(cidades);
+});
+
+app.get("/cidades/:id", (request, response) => {
+    const { id } = request.params;
+    const cidade = cidades.find((cidade) => cidade.id === id);
+    return response.json(cidade);
+});
+
+app.put("/cidades/:id", (request, response) => {
+    const { id } = request.params;
+    const { name, estado_id } = request.body;
+
+    const cidadeIndex = cidades.findIndex((cidade) => cidade.id === id);
+    cidades[cidadeIndex] = {
+        ...cidades[cidadeIndex],
+        name, 
+        estado_id,
+    };
+
+    cidadeFile();
+    return response.json({ message: "Cidade Alterada com Sucesso" });
+});
+
+app.delete("/cidades/:id", (request, response) => {
+    const { id } = request.params;
+
+    const cidadeIndex = cidades.findIndex((cidade) => cidade.id === id);
+
+    cidades.splice(cidadeIndex, 1);
+    cidadeFile();
+    return response.json({ message: "Cidade Removida com Sucesso"});
+
+});
+
+function cidadeFile() {
+
+    writeFile("cidades.json", JSON.stringify(cidades), (err) => {
+        if (err) {
+            console.log(err)
+        }else {
+            console.log("Cidade Inserida");
+        }
+    });
+};
 
 app.listen(4002, () => console.log("Servidor rodando na porta 4002"));
