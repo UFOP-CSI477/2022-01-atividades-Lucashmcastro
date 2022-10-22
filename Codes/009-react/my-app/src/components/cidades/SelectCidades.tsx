2bf52113dc7725c15721121829d03721a334d39d
Lucashmcastro <lucashmcastro@outlook.com>
2022-10-22 01:37:50 -0300

    Criado Components Entidades Estados e Cidades

---
+++ Codes/009-react/my-app/src/components/cidades/SelectCidades.tsx
@@ -0,0 +1,52 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import api from "../../services/api";
+import { CidadeModel } from "./ListCidades";
+
+interface SelectCidadesProps {
+    id: number;
+    setId: Dispatch<SetStateAction<number>>;
+}
+
+const SelectCidades = (props: SelectCidadesProps) => {
+
+    const [cidades, setCidades] = useState<CidadeModel[]>([]);
+
+    // const [ id, setId ] = useState(0)
+
+    useEffect(() => {
+
+        api.get('/cidades')
+            .then(response => {
+                setCidades(response.data);
+            })
+
+    }, []);
+
+    return (
+
+        <div>
+            <div>
+                <label htmlFor="cidade">Selecione a Cidade:</label>
+            </div>
+            <div>
+                <select name="cidade"
+                    id="cidade"
+                    value={props.id}
+                    onChange={e => props.setId(parseInt(e.target.value))}>
+
+                    {
+                        cidades.map(item => (
+                            <option value={item.id}>{item.nome}-{item.sigla}</option>
+                        ))
+                    }
+
+                </select>
+            </div>
+        </div>
+
+    );
+
+
+}
+
+export default SelectCidades;
---
+++ Codes/009-react/my-app/src/routes.tsx
@@ -0,0 +1,45 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+//import CreateCidade from './components/cidades/CreateCidade';
+//import ListCidades from "./components/cidades/ListCidades";
+//import ShowCidade from "./components/cidades/ShowCidade";
+//import UpdateCidade from "./components/cidades/UpdateCidade";
+import CreateEstado from "./components/estados/CreateEstado";
+import ListEstados from "./components/estados/ListEstados";
+import ShowEstado from "./components/estados/ShowEstado";
+import UpdateEstado from "./components/estados/UpdateEstado";
+import Header from '../src/components/header/Header';
+//import LoginUser from "./components/users/LoginUser";
+
+const AppRoutes = () => {
+
+    const userName = window.localStorage.getItem('nome') || undefined;
+
+    return(
+
+        <BrowserRouter>
+
+            <Header name={userName} />
+
+            <Routes>
+
+                <Route path="/" element={ <App /> } />
+
+                <Route path="/estados" element={<ListEstados />} />
+
+                <Route path="/estados/create" element={<CreateEstado />} />
+
+                <Route path="/estados/show/:id" element={<ShowEstado /> } />
+
+                <Route path="/estados/update/:id" element={<UpdateEstado />} />
+
+
+            </Routes>
+
+        </BrowserRouter>
+
+    );
+
+}
+
+export default AppRoutes;
